e922472f7fe13b02d267dc3acfab4fa8e0fae8bb
John Bogil <johncbogil@gmail.com>
2017-01-29 18:33:05 -0500

    Initial commit

---
+++ AdminRig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdminRig
+//
+//  Created by John Bogil on 1/29/17.
+//  Copyright © 2017 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
